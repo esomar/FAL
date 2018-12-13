e9dd0f5cef9550c6dcf0416be2462fb4c325b17c
Karim Hisham <khm.karimhisham@gmail.com>
2018-12-13 13:03:20 +0200

    Revert "1.2"
    This reverts commit c991e57b6963ebec7846fa01c570d4582de29252.

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-// Read the port from the underlying environment.
-// If not exist, use the default port: 8080
-var port = process.env.VCAP_APP_PORT || 8080;
-// Create the server and listen to requests on the specified port.
-http.createServer(function (request, response) {
-}).listen(port);
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
